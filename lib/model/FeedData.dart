4c8c810b2228eae276629dc59e4b466e2add4ff7
kang <kjxstar@gmail.com>
2019-02-03 22:41:31 +0800

    finish feed data

--- lib/model/FeedData.dart
+++ lib/model/FeedData.dart
@@ -1,7 +1,17 @@
-class FeedData{
-  
+class FeedData {
+  String title;
+  List<String> images;
+  bool multiPic;
+  int type;
+  int id;
 
-  FeedData.fromJson(Map<String,dynamic> map){
+  FeedData(this.title, this.images, this.multiPic, this.type, this.id);
 
+  FeedData.fromJson(Map<String, dynamic> map) {
+    title = map['title'];
+    images = List<String>.from(map['images']);
+    multiPic = map['multipic'];
+    type = map['type'];
+    id = map['id'];
   }
 }
